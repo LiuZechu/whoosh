54edced4d0337627b96f5a5ad3b0414dc0f0d8d9
Liu Zechu <liuzechu2013@gmail.com>
2020-09-21 17:53:36 +0800

    fix duplicated scripts in index.html page

--- whoosh/lib/generated_plugin_registrant.dart
+++ whoosh/lib/generated_plugin_registrant.dart
@@ -5,8 +5,8 @@
 // ignore: unused_import
 import 'dart:ui';
 
-import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_analytics_web/firebase_analytics_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -15,8 +15,8 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
-  FirebaseAuthWeb.registerWith(registry.registrarFor(FirebaseAuthWeb));
   FirebaseAnalyticsWeb.registerWith(registry.registrarFor(FirebaseAnalyticsWeb));
+  FirebaseAuthWeb.registerWith(registry.registrarFor(FirebaseAuthWeb));
   FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
   FluttertoastWebPlugin.registerWith(registry.registrarFor(FluttertoastWebPlugin));
   UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
