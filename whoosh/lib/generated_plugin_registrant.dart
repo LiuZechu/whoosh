b6bdd00b01a84b1165049f019dd2870c03a6c159
hoholyin <holyin97@gmail.com>
2020-09-21 15:54:38 +0800

    add firebase analytics

--- whoosh/lib/generated_plugin_registrant.dart
+++ whoosh/lib/generated_plugin_registrant.dart
@@ -6,6 +6,7 @@
 import 'dart:ui';
 
 import 'package:firebase_analytics_web/firebase_analytics_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
@@ -14,6 +15,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
   FirebaseAnalyticsWeb.registerWith(registry.registrarFor(FirebaseAnalyticsWeb));
+  FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
   FluttertoastWebPlugin.registerWith(registry.registrarFor(FluttertoastWebPlugin));
   UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
   registry.registerMessageHandler();
