b397a81949192e6c29543d9486b70d39a03ece64
Liu Zechu <liuzechu2013@gmail.com>
2021-08-22 22:26:59 +0800

    update URL in QR code generation

--- whoosh/lib/util/UrlUtil.dart
+++ whoosh/lib/util/UrlUtil.dart
@@ -9,12 +9,12 @@ class UrlUtil {
   }
 
   static String generateEntireQueueUrl(int restaurantId, int groupId, String groupKey) {
-    return 'https://hoholyin.github.io/whoosh/#' + generateQueueUrl(restaurantId, groupId, groupKey);
+    return 'https://whoosh-frontend-deploy.pages.dev/#' + generateQueueUrl(restaurantId, groupId, groupKey);
   }
 
   static String generateQrCodeUrl(int restaurantId) {
     String googleQrCodeUrl = 'https://chart.googleapis.com/chart?cht=qr&chs=200x200&chl=';
-    String hostUrl = 'https%3A%2F%2Fhoholyin.github.io%2Fwhoosh%2F%23%2FjoinQueue%3Frestaurant_id%3D${restaurantId}';
+    String hostUrl = 'https%3A%2F%2Fwhoosh-frontend-deploy.pages.dev%2F%23%2FjoinQueue%3Frestaurant_id%3D${restaurantId}';
     String qrCodeUrl = googleQrCodeUrl + hostUrl;
     return qrCodeUrl;
   }
